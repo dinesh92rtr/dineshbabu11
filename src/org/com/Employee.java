31f73ba507f6b3d77f7501748f8d93f4d6294a72
dinesh92rtr <dineshasokan10@gmail.com>
2021-12-13 21:44:36 -0800

    arun task done

--- src/org/com/Employee.java
+++ src/org/com/Employee.java
@@ -1,5 +1,17 @@
 package org.com;
 
 public class Employee {
+	public void add() {
+		System.out.println("add");
+
+	}
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.add();
+	}
+	public void mul() {
+		System.out.println("mul");
+
+	}
 
 }
